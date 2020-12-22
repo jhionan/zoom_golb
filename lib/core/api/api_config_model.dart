0df3f4a37be47eb66cf4c5cee830f45087555c71
Jhionan Rian Lara dos Santos <jhionan@gmail.com>
2020-12-22 02:03:44 -0300

    feat(login): add api configuration asset and minimal api configuration

---
+++ lib/core/api/api_config_model.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'api_config_model.g.dart';
+
+@JsonSerializable()
+class ApiConfigModel {
+  String baseNews;
+  String baseDummy;
+  @JsonKey(name: "api-id")
+  String appId;
+  ApiConfigModel({
+    this.baseNews,
+    this.baseDummy,
+    this.appId,
+  });
+
+  factory ApiConfigModel.fromJson(Map<String, Object> json) =>
+  _$ApiConfigModelFromJson(json);
+  Map<String, Object> toJson() => _$ApiConfigModelToJson(this);
+}
---
+++ test/core/api/api_config/api_provider_test.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:zoom_golb/core/api/api_config_model.dart';
+import 'package:zoom_golb/core/api/api_provider.dart';
+import 'package:zoom_golb/core/api/base_api.dart';
+
+main() {
+  test('ApiConfigModel Serialization', () {
+    String apiConfigJson = '''{
+    "base_news" : "https://gb-mobile-app-teste.s3.amazonaws.com/",
+    "base_dummy" : "https://dummyapi.io/data/api/",
+    "app-id" : "5fe109855491ba26df85ad3c" 
+}''';
+
+    expect(
+        ApiConfigModel.fromJson(jsonDecode(apiConfigJson)),
+        isA<ApiConfigModel>().having((apiConfig) => apiConfig.baseNews,
+            'base news url', 'https://gb-mobile-app-teste.s3.amazonaws.com/'));
+  });
+  testWidgets('create ApiConfig from json asset', (tester) async {
+    final container = ProviderContainer();
+    ApiConfigModel config = await container.read(apiConfigProvider.future);
+    expect(
+        config,
+        isA<ApiConfigModel>().having((apiConfig) => apiConfig.baseNews,
+            'base news url', 'https://gb-mobile-app-teste.s3.amazonaws.com/'));
+  });
+
+  testWidgets('create BaseApi', (tester) async {
+    final container = ProviderContainer();
+    BaseApi _api = await container.read(apiProvider.future);
+    expect(
+        _api,
+        isA<BaseApi>().having((baseApi) => baseApi.instance,
+            'Instance of dio must not be null', isNotNull));
+  });
+}
