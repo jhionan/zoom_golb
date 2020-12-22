a72a92b86ce9b8675b571661920f6a76f7bfd912
Jhionan Rian Lara dos Santos <jhionan@gmail.com>
2020-12-22 02:07:07 -0300

    feat(api): create api provider ad base api

---
+++ lib/core/api/base_api.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:zoom_golb/core/api/api_config_model.dart';
+
+typedef ApiFactoryDelegate = Future<BaseApi> Function();
+
+class BaseApi {
+  BaseApi(this._apiconfig) {
+    _dio = Dio();
+    _dio.options.baseUrl = _apiconfig.baseNews;
+    ready.add(true);
+  }
+
+  final ApiConfigModel _apiconfig;
+  Dio _dio;
+  static BehaviorSubject<bool> ready = BehaviorSubject.seeded(false);
+
+  dispose() {
+    ready.close();
+  }
+
+  Dio get instance => _dio;
+}
+
+mixin Api {
+  
+}
