216739a4d0ff709327f6df7ec47848d27de0e567
Jhionan Rian Lara dos Santos <jhionan@gmail.com>
2020-12-21 23:37:27 -0300

    feat(structure): create basic folder stucture

---
+++ lib/core/base_bloc/base_bloc.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+import 'package:rxdart/subjects.dart';
+
+abstract class BaseBloc<IN extends BaseEvent, STATE extends BaseState> {
+  Stream<STATE> get event$;
+  StreamSink<IN> get inEvent;
+  dispose();
+}
+
+abstract class Bloc<IN extends BaseEvent, STATE extends BaseState>
+    implements BaseBloc<IN, STATE> {
+  Bloc() {
+    _eventSubscription = inEventStream$.listen(onData, onError: onError);
+    _stateSubscription = event$.listen((event) {
+      lastState = event;
+    }, onError: onError);
+  }
+  STATE lastState;
+  StreamSubscription<STATE> _stateSubscription;
+  StreamSubscription<IN> _eventSubscription;
+
+  @override
+  Stream<STATE> get event$ => eventController.stream;
+  @override
+  StreamSink<IN> get inEvent => inEventController.sink;
+  Stream get inEventStream$ => inEventController.stream;
+
+  StreamController<IN> inEventController = StreamController();
+  BehaviorSubject<STATE> eventController = BehaviorSubject();
+
+  @override
+  @mustCallSuper
+  dispose() {
+    _eventSubscription?.cancel();
+    _stateSubscription.cancel();
+    inEventController.close();
+    eventController.close();
+  }
+
+  onData(IN event);
+
+  onError(Object error, StackTrace stackTrace) {
+    print('Stream Error $error');
+    print('STACK TRACE $stackTrace');
+  }
+}
+
+mixin OutEvent<IN extends BaseEvent, STATE extends BaseState,
+    OUT extends BaseOutEvent> on Bloc<IN, STATE> {
+  StreamController<OUT> _outEventController = StreamController.broadcast();
+
+  Stream<OUT> get outEvent => _outEventController.stream;
+
+  void emitEvent(OUT event) {
+    _outEventController.add(event);
+  }
+
+  @override
+  dispose() {
+    _outEventController.close();
+    super.dispose();
+  }
+}
+
+class BaseEvent {}
+
+class BaseState {}
+
+class BaseOutEvent {}
---
+++ lib/core/navigation/app_routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class AppRoutes {
+  static const String home = '/';
+  static const String login = '/login';
+  static const String feed = '/feed';
+  static Route<Object> onGenerateRoute(RouteSettings settings) {
+    Widget page;
+    switch (settings.name) {
+      case home:
+    }
+    return MaterialPageRoute(builder: (context) => page);
+  }
+}
