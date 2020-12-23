eaa4f0639b0e80cba7257a6989b754f1aa28b6d7
Jhionan Rian Lara dos Santos <jhionan@gmail.com>
2020-12-23 19:14:20 -0300

    refactor(api-provider): change provider functions to be encapsulated inside a class

--- lib/core/api/api_provider.dart
+++ lib/core/api/api_provider.dart
@@ -7,15 +7,18 @@ import 'package:meta/meta.dart';
 import 'package:zoom_golb/core/api/api_config_model.dart';
 import 'package:zoom_golb/core/api/base_api.dart';
 
-@visibleForTesting
-final apiConfigProvider = FutureProvider<ApiConfigModel>((ref) async {
-  final Map<String, Object> content = json.decode(
-    await rootBundle.loadString('assets/configuration/api_config.json'),
-  );
-  return ApiConfigModel.fromJson(content);
-});
+class ApiProvider {
+  ApiProvider._();
+  @visibleForTesting
+  static final apiConfigProvider = FutureProvider<ApiConfigModel>((ref) async {
+    final Map<String, Object> content = json.decode(
+      await rootBundle.loadString('assets/configuration/api_config.json'),
+    );
+    return ApiConfigModel.fromJson(content);
+  });
 
-final apiProvider = FutureProvider<BaseApi>((ref) async {
-  ApiConfigModel config = await ref.watch(apiConfigProvider.future);
-  return BaseApi(config);
-});
+  static final apiProvider = FutureProvider<BaseApi>((ref) async {
+    ApiConfigModel config = await ref.watch(apiConfigProvider.future);
+    return BaseApi(config);
+  });
+}
